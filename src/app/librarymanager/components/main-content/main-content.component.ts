a607c99421562c3dd2b63d03d49b306dbd00f045
Sanket A Darwante <sanketdarwante6@gmail.com>
2021-10-04 16:55:45 -0400

    main-content component done

--- src/app/librarymanager/components/main-content/main-content.component.ts
+++ src/app/librarymanager/components/main-content/main-content.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { Anotomy } from '../../models/anotomy';
+import { MedLibraryData } from '../../models/medlibrarydata';
+import { LibraryService } from '../../services/library.service';
+import { InfodailogComponent } from '../infodailog/infodailog.component';
 
 @Component({
   selector: 'app-main-content',
@@ -7,9 +14,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContentComponent implements OnInit {
 
-  constructor() { }
+  medLibData!: Observable<MedLibraryData>;
+  displayedColumns: string[] = ['id', 'name'];
+  anotomyList: Array<Anotomy> = [];
+  dataSource!: MatTableDataSource<Anotomy>;
+
+  constructor(
+    private libraryService: LibraryService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+
+    this.medLibData = this.libraryService.medLibraryData;
+    this.libraryService.loadAll();
+    this.medLibData.subscribe( data => {
+      console.log(data);
+      this.buildUiModal(data);
+    },
+     error=> {
+       console.log('Error occured main content load:', error);
+     })
+  }
+
+  buildUiModal(medLibData: MedLibraryData) {    
+    for (let row of medLibData.data) {
+      for(let anotomy of row.anatomical_structures) {
+        this.anotomyList.push({ id: anotomy.id,name: anotomy.name });   
+      }
+    }
+
+    this.anotomyList = this.anotomyList.filter(
+      (thing, i, arr) => arr.findIndex(t => t.name === thing.name) === i
+    );
+    console.log("This is list:",this.anotomyList[0]);
+    this.dataSource = new MatTableDataSource<Anotomy>(this.anotomyList);
+  }
+
+  openDialog(id: string) {
+    id = id.replace(':','_');
+    console.log("clicked id:", id);
+    let dialogRef = this.dialog.open(InfodailogComponent, {
+      width: '250px',
+      data: id,
+    });
+    
+
   }
 
 }
